f96e9341510068bd07394ae75a0324dc5fa44e3c
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-06-28 23:57:40 +0200

    don't use raw strings

--- .tests/tests.py
+++ .tests/tests.py
@@ -6,7 +6,7 @@
 # micha.birklbauer@gmail.com
 
 def test1():
-    expected = r"Starting Template App v0.0.1 ...\r\nHello Luna!\r\nSuccessfully greeted Luna!\r\nTemplate App exited!\r\n"
+    expected = "Starting Template App v0.0.1 ...\r\nHello Luna!\r\nSuccessfully greeted Luna!\r\nTemplate App exited!\r\n"
 
     import subprocess
     result = subprocess.run(["./App", "--name", "Luna"], stdout = subprocess.PIPE)
@@ -14,7 +14,7 @@ def test1():
     assert result.stdout.decode("utf-8") == expected
 
 def test2():
-    expected = r"Starting Template App v0.0.1 ...\r\nHi Luna!\r\nSuccessfully greeted Luna!\r\nTemplate App exited!\r\n"
+    expected = "Starting Template App v0.0.1 ...\r\nHi Luna!\r\nSuccessfully greeted Luna!\r\nTemplate App exited!\r\n"
 
     import subprocess
     result = subprocess.run(["./App", "--name", "Luna", "--greeting", "Hi"], stdout = subprocess.PIPE)
