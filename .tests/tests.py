78921331978b1f6a54c5c4ba7571b851ca87b62b
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-06-29 00:09:17 +0200

    fix tests

--- .tests/tests.py
+++ .tests/tests.py
@@ -6,23 +6,23 @@
 # micha.birklbauer@gmail.com
 
 def test1():
+    expected = "Starting Template App v0.0.1 ... Hello Luna! Successfully greeted Luna! Template App exited!".split()
+
     import subprocess
     result = subprocess.run(["./App", "--name", "Luna"], stdout = subprocess.PIPE)
     print(f"GOT$\n{result.stdout.decode("utf-8")}$")
     lines = result.stdout.decode("utf-8").split()
-    assert len(lines) == 4
-    assert lines[0].strip() == "Starting Template App v0.0.1 ..."
-    assert lines[1].strip() == "Hello Luna!"
-    assert lines[2].strip() == "Successfully greeted Luna!"
-    assert lines[3].strip() == "Template App exited!"
+    assert len(lines) == len(expected)
+    for i in range(len(expected)):
+        assert lines[i].strip() == expected[i].strip()
 
 def test2():
+    expected = "Starting Template App v0.0.1 ... Hi Luna! Successfully greeted Luna! Template App exited!".split()
+
     import subprocess
     result = subprocess.run(["./App", "--name", "Luna", "--greeting", "Hi"], stdout = subprocess.PIPE)
     print(f"GOT$\n{result.stdout.decode("utf-8")}$")
     lines = result.stdout.decode("utf-8").split()
-    assert len(lines) == 4
-    assert lines[0].strip() == "Starting Template App v0.0.1 ..."
-    assert lines[1].strip() == "Hi Luna!"
-    assert lines[2].strip() == "Successfully greeted Luna!"
-    assert lines[3].strip() == "Template App exited!"
+    assert len(lines) == len(expected)
+    for i in range(len(expected)):
+        assert lines[i].strip() == expected[i].strip()
